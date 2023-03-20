8e4d5f731aecc1e8c604b6d6c62e12c87640051d
SofiaValadares <svc3@cesar.school>
2023-03-20 09:49:14 -0300

    Co-authored-by: Matheus Gomes de Andrade <MatheusGom@users.noreply.github.com>

--- Fazendo/Feed_de_produtos/teste.py
+++ Fazendo/Feed_de_produtos/teste.py
@@ -23,19 +23,6 @@ def cat(escolha):
     else:
         print("\n\nValor Inválido")
 
-
-def pes(pesquisa):
-    clear_console()
-
-    print("\n\n")
-    print("Schoolyard Finds\t", "[        ", pesquisa, "        ]", "\t Lista de desejos | Configurações | Perfil")
-
-    print("|                |       |                |      |                |      |                |")
-    print("|                |       |                |      |                |      |                |")
-    print("|    Produto 1   |       |    Produto 2   |      |    Produto 3   |      |    Produto 4   |")
-    print("|                |       |                |      |                |      |                |")
-    print("|                |       |                |      |                |      |                |")
-
 clear_console()
 
 categorias = ["0.arte", "1.utilitarios", "2.tecnologia", "3.alimenticio"]
@@ -48,24 +35,16 @@ for i in range(4):
     print("\n\n", categorias[i])
 
     print("|                |       |                |      |                |      |                |")
-    print("|    Produto 1   |       |    Produto 2   |      |    Produto 3   |      |    Produto 4   |")
+    print("|    Produto", i,"  |       |    Produto", i+1,"  |      |    Produto", i+2,"  |      |    Produto", i+3,"  |")
     print("|                |       |                |      |                |      |                |")
 
     i +=1
 
 print("0 - Escolher uma Categoria")
-print("1 - Pesquisar")
-print("2 - Clicar em Produto")
+print("1 - Clicar em Produto")
 
 a = int(input("O que desejas fazer?"))
 
 if a == 0:
     escolha = int(input("Selecione uma categoria por número: "))
     cat(escolha)
-
-elif a == 1:
-    pesquisa = str(input("O que você deseja pesquisar: "))
-    pes(pesquisa)
-
-elif a == 2:
-    prod = int(input("Qual produto você deseja clicar: "))
