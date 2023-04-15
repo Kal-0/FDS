05d4a622384adabeba589b149d116aaa4a029b92
SofiaValadares <svc3@cesar.school>
2023-04-15 12:01:14 -0300

    Sofia - algumas mudanças do model de produto

--- SYF_Django/SchoolYardFinds/publicacao/admin.py
+++ SYF_Django/SchoolYardFinds/publicacao/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from publicacao.models import Produto
+
+class ListandoProduto(admin.ModelAdmin):
+    list_display =("id", "nome", "categoria", "estoque")
+    list_display_links =("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("estoque",)
+    list_per_page = 10
+
+admin.site.register(Produto, ListandoProduto)
+
 # Register your models here.
--- SYF_Django/SchoolYardFinds/publicacao/models.py
+++ SYF_Django/SchoolYardFinds/publicacao/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
 
 class Produto(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("PROMOÇÃO", "Promoção"),
+        ("PARA VOCÊ", "Para você"),
+    ]
+
     nome            = models.CharField(max_length=50)
     preco           = models.DecimalField(decimal_places=2, max_digits=10000)
+    # botoar uma referencia para o id do usuario mas n sei como
     descricao       = models.TextField(default="Ecreva sua descrição", max_length= 150, blank= True)
-    produto_imagem  = models.ImageField(upload_to="images/", blank=True , null=True)
+    categoria       = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='') #categoria do produto
+    produto_imagem  = models.ImageField(upload_to="images/%Y/%m/%d/", blank=True , null=True) #podemos botar uma data para evitar erros de uma foto com mesmo nome
+    estoque         = models.BooleanField(default=True) #se ainda tem no estoque
+
+    def __str__(self):
+        return f"Produto [nome={self.nome}]"
