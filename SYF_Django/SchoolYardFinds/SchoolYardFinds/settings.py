ecbcfa6f06be5158b971d89791335e1f60e93a0b
Matheus Gomes de Andrade <mga@cesar.school>
2023-05-06 11:06:13 -0300

    Botão de Logout funcional adicionado e correção do botão de voltar em criando_publicacao

--- SYF_Django/SchoolYardFinds/SchoolYardFinds/settings.py
+++ SYF_Django/SchoolYardFinds/SchoolYardFinds/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'login'
--- SYF_Django/SchoolYardFinds/home/urls.py
+++ SYF_Django/SchoolYardFinds/home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from home.views import home, feed, perfil, login_user, signup, create_item, page_categoria, produto_detalhes
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('home/', home, name='home'),#2
@@ -10,4 +11,5 @@ urlpatterns = [
     path('publicacao/', create_item, name='create_item'),
     path('categoria/<int:foto_id>', page_categoria, name='categoria'),
     path('produto/<int:foto_id>', produto_detalhes, name='produto'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
