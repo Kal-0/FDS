2ee583becab5e0a9959624642ddbc1ec0c865793
SofiaValadares <svc3@cesar.school>
2023-05-15 20:34:53 -0300

    static collect

--- SYF_Django/SchoolYardFinds/SchoolYardFinds/settings.py
+++ SYF_Django/SchoolYardFinds/SchoolYardFinds/settings.py
@@ -117,17 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
-#STATIC_ROOT = 'static'
 
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'SchoolYardFinds/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
