acfda335fb3d6afc36afe60c381da1e55858df76
Caio <hiratelli.0@gmail.com>
2023-05-28 13:30:25 -0300

    testando CI 3.5

--- tests.py
+++ tests.py
@@ -24,13 +24,19 @@ driver_path = r"TestDrivers/chromedriver"
 timeout = 10
 
 options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+
 
 
 
 global driver
 
-driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
 #driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
 
 driver.set_page_load_timeout(timeout)
 
